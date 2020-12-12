c0d2859b0171bb967e902f9784e00f293c91275f
bayan79 <bayandink@yandex.ru>
2020-12-12 21:56:52 +0500

    laba 6

---
+++ lab6.py
@@ -0,0 +1,8 @@
+# 1) Описание себя самого через список
+self_list = ['Kirill', 'Bayandin', 22, "88005553535"]
+print(self_list)
+
+# 2) Описание себя самого через словарь
+fields = ['name', 'surname', 'age', 'phone']
+self_dict = dict(zip(fields, self_list))
+print(self_dict)
